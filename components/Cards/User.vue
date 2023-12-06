576c3fa44b607ecc570e6a2b083aa7a004293fd0
Aure6 <aurelien7112001@gmail.com>
2023-12-06 19:41:08 +0100

    The diff adds a new component called `CardsUser` that displays a user's avatar, full name, and information. It also adds three instances of the `CardsUser` component to the page, each with different configurations for the avatar slot.

--- components/Cards/User.vue
+++ components/Cards/User.vue
@@ -16,15 +16,23 @@ defineProps({
     type: String,
     required: false,
   },
+  src: {
+    type: String,
+    required: true,
+  },
+  alt: {
+    type: String,
+    required: false,
+  },
 });
 </script>
 
 <template>
   <NuxtLink :to="to"
     class="flex flex-col p-4 max-w-xs group hover:bg-gray-200/75 transition items-center justify-center ring-2 ring-gray-200/75 my-8 space-y-2"
-    :avatar="avatar" :info="info">
-    <div class="text-5xl text-gray-400 group-hover:text-gray-800 transition">
-      <slot name="avatar" />
+    :avatar="avatar" :info="info" :src="src" :alt="alt">
+    <div class=" text-5xl text-gray-400 group-hover:text-gray-800 transition">
+      <img :src=src alt="">
     </div>
 
     <div class="text-xl text-gray-100 group-hover:text-black group-hover:underline transition">
@@ -35,3 +43,20 @@ defineProps({
     </div>
   </NuxtLink>
 </template>
+
+<!-- <template>
+  <NuxtLink :to="to"
+    class="flex flex-col p-4 max-w-xs group hover:bg-gray-200/75 transition items-center justify-center ring-2 ring-gray-200/75 my-8 space-y-2"
+    :avatar="avatar" :info="info" :src="src" :alt="alt">
+    <div class=" text-5xl text-gray-400 group-hover:text-gray-800 transition">
+      <slot name="avatar" />
+    </div>
+
+    <div class="text-xl text-gray-100 group-hover:text-black group-hover:underline transition">
+      <slot name="fullname" />
+    </div>
+    <div class="block text-gray-100 group-hover:text-black group-hover:underline transition">
+      <slot name="info" />
+    </div>
+  </NuxtLink>
+</template> -->
--- pages/docs/composants/userCards.vue
+++ pages/docs/composants/userCards.vue
@@ -16,21 +16,50 @@ const code = `<TextesCode code="${consoleLog}" language="js" />`;
       blocks de code, etc.
     </TextesParagraphe>
 
-    <CardsUser>
-      TODO
-      <template v-slot:avatar>
-        <!-- content for the avatar slot -->
-        <img src="../../../public/img/test.png" alt="">
-      </template>
-      <template #fullname>
-        <!-- content for the info slot -->
-        John Doe
-      </template>
-      <template #info>
-        <!-- content for the info slot -->
-        web developper since 2015
-      </template>
-    </CardsUser>
+    <div class="flex">
+      <CardsUser>
+        <template v-slot:avatar>
+          <!-- content for the avatar slot -->
+          <img src="../../../public/img/test.png" alt="">
+        </template>
+        <template #fullname>
+          <!-- content for the info slot -->
+          John Doe
+        </template>
+        <template #info>
+          <!-- content for the info slot -->
+          web developper since 2015
+        </template>
+      </CardsUser>
+      <CardsUser>
+        <template v-slot:avatar src="../../../public/img/test.png">
+          <!-- content for the avatar slot -->
+        </template>
+        <template #fullname>
+          <!-- content for the info slot -->
+          John Doe
+        </template>
+        <template #info>
+          <!-- content for the info slot -->
+          web developper since 2015
+        </template>
+      </CardsUser>
+      <CardsUser>
+        <template v-slot:avatar>
+          <!-- content for the avatar slot -->
+          <img src="../../../public/img/test.png" alt="">
+        </template>
+        <template #fullname>
+          <!-- content for the info slot -->
+          John Doe
+        </template>
+        <template #info>
+          <!-- content for the info slot -->
+          web developper since 2015
+        </template>
+      </CardsUser>
+    </div>
+
 
     <TextesCode :code="`<TextesParagraphe>
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam voluptatum, quibusdam, voluptates quia quae voluptatem, quos quod quas voluptatibus quidem doloribus. Quisquam voluptatum, quibusdam, voluptates quia quae voluptatem, quos quod quas voluptatibus quidem doloribus.
